a9670a5c9f2cbe79850843ddc88a61ea7ae76ae9
Alice Fung <alice.o.fung@gmail.com>
2015-06-15 01:12:54 -0400

    add input check

--- towers.rb
+++ towers.rb
@@ -2,16 +2,16 @@ class TowerOfHanoi
 
   attr_accessor :tower_height, :goal_tower, :tower_a, :tower_b, :tower_c
 
-  def initialize
+  def initialize(height)
     #create tower height
-    @tower_a = (1..@tower_height).to_a.reverse
+    @tower_a = (1..height).to_a.reverse
     @goal_tower = @tower_a.dup
     @tower_b, @tower_c = [], []
   end
 
-  def instructions
-  puts "Welcome to Tower of Hanoi!"
-  puts "Instructions: Enter where you'd like to move from and to in the format [a,b]. Enter 'q' to quit."
+  def introduction
+    @instruction = "Enter where you'd like to move the top disk from and to in the format: a,b. Enter 'q' to quit."
+    puts "Welcome to Tower of Hanoi!" + "/n @instruction"
   end
 
   def rules
@@ -22,42 +22,76 @@ class TowerOfHanoi
 
   def quit
     #end game
-    puts "Aww. Ok, goodbye."
+    if game_won?
+      render
+      puts "Congratulations! You did it!"
+    else
+      puts "Aww. Ok, goodbye."
+    end
     exit
   end
 
   def game_won?
     #test if won
-    if @tower_c == @goal_tower || @tower_b == @goal_tower
-      puts "Congratulations! You did it!"
-      exit
+    (@tower_c == @goal_tower || @tower_b == @goal_tower) ? true : false
+  end
+
+  def errors(type)
+    #put errors
+  end
+
+  def valid_input?(input)
+    #check if user input is valid & quit option
+    towers = ["a", "b", "c"]
+    pass = false
+    if input == "q"
+      quit
+    elsif towers.include? input[0] && towers.include? input[2]
+      pass = true
     else
-      false
+      puts "I do not understand. #{@instruction}"
+    end
+    pass
+  end
+
+  def valid_move?(directions)
+    #check if disk moved is smaller than the one it's stacked on
+    if #valid move
+      return true
+    else
+      rules
+      return false
     end
   end
 
-  def valid_input?
-    #check if user input is valid & quit option
-  end
+  def move_disk(move)
 
-  def valid_move?
-    #check if disk moved is smaller than the one it's stacked on
-  end
-
-  def move_disk
     #game move
+
   end
 
   def render
     #prints current state of game board
+
   end
 
   def play #game loop w/ quit option
-    #instructions
-    #board
-    #move_disk
-    #play
-    #check quit, input, move, then move, then status
+    introduction
+    rules
+    render
+    until game_won?
+      puts "Your move:"
+      move = gets.chomp
+      if valid_input?(move) && valid_move?(move)
+        move_disk(move)
+        render
+      else
+        puts @instruction
+      end
+    end
   end
 
 end
+
+test = TowerOfHanoi.new(3)
+test.play
