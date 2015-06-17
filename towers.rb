9ed5ee5478718dfbb791fd23e36b657a9d407238
Alice Fung <alice.o.fung@gmail.com>
2015-06-16 22:05:34 -0400

    added input and move checks

--- towers.rb
+++ towers.rb
@@ -13,24 +13,25 @@ class TowerOfHanoi
   end
 
   def introduction
-    @instruction = "Enter where you'd like to move the top disk from and to in the format: a,b. Enter 'q' to quit."
+    @instruction = "Enter where you'd like to move the top disk from and to in the format: a,b. \nEnter 'q' to quit."
     puts "Welcome to Tower of Hanoi!" + "\n#{@instruction}"
   end
 
   def rules
     puts "1) Only one disk can be moved at a time"
     puts "2) Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack i.e. a disk can only be moved if it is the uppermost disk on a stack."
-    puts "3) No disk may be placed on top of a smaller disk."
+    @rule_3 = "No disk may be placed on top of a smaller disk."
+    puts "3) #{@rule_3}"
   end
 
   def tower_choice(input_letter)
     case input_letter
     when "a"
-      return @tower_a
+      @tower_a
     when "b"
-      return @tower_b
+      @tower_b
     when "c"
-      return @tower_c
+      @tower_c
     end
   end
 
@@ -50,50 +51,57 @@ class TowerOfHanoi
 
   def errors(type)
     #put errors
+    case type
+    when "big_disk"
+      puts @rule_3
+    when "empty_tower"
+      puts "There is no disk in that tower to move."
+    end
   end
 
   def valid_input?(input)
     #check if user input is valid & quit option
     towers = ["a", "b", "c"]
-    check = false           #whether input is valid
+    good_input = false           #whether input is valid
     if input == "q"
       quit
-    elsif (towers.include? input[0]) && (towers.include? input[2])
-      check = true
+    elsif (towers.include? input[0]) && (towers.include? input[2]) && input.length == 3
+      good_input = true
     else
       puts "I do not understand. The towers are a, b, and c. #{@instruction}"
     end
-    check
+    good_input
   end
 
   def valid_move?(move)         #check if disk moved is smaller than the one it's stacked on
-    #disk_size = move[0].tower_choice[-1]
-    #destination_disk_size = move[2].tower_choice[-1]
-    #if origin < destination #valid move
-    #  return true
-    #else
-    #  rules
-    #  return false
-    #end
-
-    true
+    @from = tower_choice(move[0])
+    @to = tower_choice(move[2])
+    if @from.empty?
+      errors("empty_tower")
+      good_move = false
+    elsif !@to.empty? && @from[-1] > @to[-1]
+      errors("big_disk")
+      good_move = false
+    else
+      good_move = true
+    end
+    good_move
   end
 
 
   ##--GAME PLAY--##
 
-  def move_disk(move)
-    tower_choice(move[2]) << tower_choice(move[0]).pop
+  def move_disk
+    @to << @from.pop
   end
 
-  def render
-    #prints current state of game board
+  def render          #prints current state of game board
     print @tower_a
     print @tower_b
     print @tower_c
   end
 
-  def play #game loop w/ quit option
+  def play            #game loop w/ quit option
     introduction
     rules
     render
@@ -101,7 +109,7 @@ class TowerOfHanoi
       puts "Your move:"
       move = gets.downcase.chomp
       if valid_input?(move) && valid_move?(move)
-        move_disk(move)
+        move_disk
         render
       else
         puts @instruction
